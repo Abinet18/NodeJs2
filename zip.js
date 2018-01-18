15005a2c1ee5d86170d81823cccf6b542b8ed2c4
unknown <abinet.tafa@gmail.com>
2018-01-18 07:34:10 -0600

    zip update

--- zip.js
+++ zip.js
@@ -2,18 +2,25 @@ const zlib=require('zlib');
 const path=require('path');
 const fs= require('fs');
 
-const gzip = (filePath)=> {
+const gzip = (filePath,dstPath)=> {
     var readSt=fs.createReadStream(filePath);
-    var writeSt=fs.createWriteStream(filePath+".gz");
-    readSt.pipe(zlib.createGzip()).pipe(writeSt);
+    var writeSt=fs.createWriteStream(dstPath);
+    readSt.pipe(zlib.createGzip()).pipe(writeSt).on('finish',function(){
+        console.log("done");})
 };
 
-const gunzip = (filePath)=> {
+const gunzip = (filePath,dstPath)=> {
     var readSt=fs.createReadStream(filePath);
-    var writeSt=fs.createWriteStream(filePath+'gunzip');
+    var writeSt=fs.createWriteStream(dstPath);
     readSt.pipe(zlib.createGunzip()).on('error',function(error){console.log(error);}).pipe(writeSt).on('finish',
     function(){console.log('done');});
 };
 
-//gzip(__dirname+'/webServeFile.js');
-gunzip(__dirname+'/webServeFile.js.gz');
+gzip(__dirname+'/bigfile.txt',__dirname+'/bigfile.txt.gz');
+gunzip(__dirname+'/bigfile2.txt.gz',__dirname+'/bigfile2.txt');
+// var newfile=fs.createWriteStream('./bigfile.txt');
+// for(let i=0;i<10e5;i++)
+// {
+//    newfile.write("this makes a big text file");
+// }
+// newfile.end;
